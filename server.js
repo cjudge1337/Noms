3ce002023b29b58d36c2aca8e27ddeb2fffc7a96
Carson Judge <cjudge1337@gmail.com>
2017-07-25 17:46:03 -0700

    Express server is rendering index.html

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const axios = require('axios');
+const path = require('path');
 const express = require('express');
 const app = express();
+
+app.use(express.static(__dirname));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 app.get('/fetchData', (req, res) => {
   axios.get('https://jsonplaceholder.typicode.com/posts/1')
