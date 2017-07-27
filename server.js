836ba759ccb1a5b033bea69b29ff7ffc00926318
Carson Judge <cjudge1337@gmail.com>
2017-07-26 20:31:09 -0700

    Install circular-json

--- server.js
+++ server.js
@@ -1,23 +1,25 @@
 const axios = require('axios');
+const CircularJSON = require('circular-json');
 const path = require('path');
 const express = require('express');
 const app = express();
 
 app.use(express.static(__dirname));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
-
 app.get('/fetchData', (req, res) => {
-  axios.get('https://jsonplaceholder.typicode.com/posts/1')
+  axios.get('http://date.jsontest.com')
     .then((data) => {
-      res.send(data);
+      console.log(data);
+      res.json(data);
     })
     .catch(error => {
       console.error(error);
     })
-})
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 app.listen(3000, () => {
   console.log('Listening on port 3000')
